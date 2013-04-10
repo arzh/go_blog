3104dfc90558949cdda90c08a882cb559ee9cae3
Jacob <jacobaarthur@gmail.com>
2013-04-09 22:19:53 -0500

    almost working html template maps

--- src/blog.go
+++ src/blog.go
@@ -6,11 +6,12 @@ import (
 	"net/http"
 	"time"
 
-	"appengine"
-	"appengine/datastore"
+	//"appengine"
+	//"appengine/datastore"
 )
 
 func init() {
+	InitPages()
 	http.HandleFunc("/", dbTester)
 }
 
@@ -21,8 +22,7 @@ type PostDB struct {
 	Last_Mod time.Time
 }
 
-func dbTester(w http.ResponseWriter, r *http.Request) {
-	context := appengine.NewContext(r)
+func initData() []PostDB {
 
 	post := PostDB{
 		Subject:  "Hey new post",
@@ -30,22 +30,39 @@ func dbTester(w http.ResponseWriter, r *http.Request) {
 		Created:  time.Now(),
 		Last_Mod: time.Now()}
 
-	inc_key := datastore.NewIncompleteKey(context, "PostDB", nil)
+	post2 := PostDB{
+		Subject:  "Hey the second post",
+		Content:  "This is a new post that I am making to test the DB AGAIN!",
+		Created:  time.Now(),
+		Last_Mod: time.Now()}
 
-	key, err := datastore.Put(context, inc_key, &post)
+	return []PostDB{post, post2}
+}
+
+var Pages map[string]*template.Template
+
+func InitPages(w http.ResponseWriter) {
+	p := "templates/"
+	Pages = make(map[string]*template.Template)
+
+	var err error
+	Pages["front"], err = template.ParseFiles(p+"base.html", p+"front.html", p+"page.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
 	}
 
-	var post_again PostDB
-	if err := datastore.Get(context, key, &post_again); err != nil {
+}
+
+func dbTester(w http.ResponseWriter, r *http.Request) {
+	//context := appengine.NewContext(r)
+	InitPages()
+
+	posts := initData()
+
+	err := Pages["front"].Execute(w, posts)
+	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
 	}
-
-	page, _ := template.ParseFiles("templates/blog.html")
-	page.Execute(w, post_again)
 
 	//fmt.Fprintf(w, "Subject: %s | Content: %s | Created: %s", post_again.Subject, post_again.Content, post_again.Created)
 }
