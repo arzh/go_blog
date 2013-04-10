d8148784a418748ba2fc3d580e3ceea1d7b3b502
Jacob <jacobaarthur@gmail.com>
2013-04-10 00:19:06 -0500

    Refactored PostDB to Post and added a generation function to handle key and time setting | moved template defs to the templates package

--- src/blog.go
+++ src/blog.go
@@ -2,64 +2,35 @@ package blog
 
 import (
 	//"fmt"
-	"html/template"
 	"net/http"
-	"time"
 
 	//"appengine"
 	//"appengine/datastore"
+
+	"templates"
 )
 
 func init() {
-	InitPages()
+	_ = templates.InitPages()
 	http.HandleFunc("/", dbTester)
 }
 
-type PostDB struct {
-	Subject  string
-	Content  string
-	Created  time.Time
-	Last_Mod time.Time
-}
+func initData() []Post {
 
-func initData() []PostDB {
+	post := NewPost("Hey new post", "This is a new post that I am making to test the DB")
 
-	post := PostDB{
-		Subject:  "Hey new post",
-		Content:  "This is a new post that I am making to test the DB",
-		Created:  time.Now(),
-		Last_Mod: time.Now()}
+	post2 := NewPost("Hey the second post", "This is a new post that I am making to test the DB AGAIN!")
 
-	post2 := PostDB{
-		Subject:  "Hey the second post",
-		Content:  "This is a new post that I am making to test the DB AGAIN!",
-		Created:  time.Now(),
-		Last_Mod: time.Now()}
-
-	return []PostDB{post, post2}
-}
-
-var Pages map[string]*template.Template
-
-func InitPages(w http.ResponseWriter) {
-	p := "templates/"
-	Pages = make(map[string]*template.Template)
-
-	var err error
-	Pages["front"], err = template.ParseFiles(p+"base.html", p+"front.html", p+"page.html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-
+	return []Post{post, post2}
 }
 
 func dbTester(w http.ResponseWriter, r *http.Request) {
 	//context := appengine.NewContext(r)
-	InitPages()
+	//InitPages()
 
 	posts := initData()
 
-	err := Pages["front"].Execute(w, posts)
+	err := templates.Pages["front"].Execute(w, posts)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
